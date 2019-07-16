32574823f79c1290aa134286416418d3a39997b7
Jeremy Landis <jeremylandis@hotmail.com>
2019-07-16 02:49:52 -0400

    [ci] Add back in double bracket in case.

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -22,7 +22,7 @@
 # 3. Deploy site
 #    a. Use -q option to only display Maven errors and warnings.
 
-if [ $TRAVIS_REPO_SLUG == "spotbugs/spotbugs-maven-plugin" ] && [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "spotbugs" ] && [ "$TRAVIS_COMMIT_MESSAGE" != *"[maven-release-plugin]"* ]; then
+if [ $TRAVIS_REPO_SLUG == "spotbugs/spotbugs-maven-plugin" ] && [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "spotbugs" ] && [[ "$TRAVIS_COMMIT_MESSAGE" != *"[maven-release-plugin]"* ]]; then
 
   if [ $TRAVIS_JDK_VERSION == "oraclejdk8" ]; then
 
